ac5f77219034a2161ef7dc770a966fa9ce36b015
Joel Mann <joelmann@fb.com>
2022-01-28 23:21:15 +0000

    Implementing a template for the HTML file

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,17 +2,11 @@ package 'httpd' do
     action :install
 end
 
-file '/var/www/html/index.html' do
+template '/var/www/html/' do
+    source 'index.html.erb'
     action :create
-    content "<html><h1>Hello, world!</h1>
-    <p> This computer is the following:
-    Hostname: #{node['hostname']}
-    IPAddress: #{node['ipaddress']}
-    CPU MHz: #{node['cpu']['0']['mhz']}
-    Memory: #{node['memory']['total']}
-    </p>
-    </html>"
 end
+
 
 service 'httpd' do
     action [ :enable, :start ]
