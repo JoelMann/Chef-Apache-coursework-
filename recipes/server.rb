4ca33a943021bd72fffabd0197bdb1136dbe17ef
Joel Mann <jmann.creston@gmail.com>
2022-02-03 17:43:34 +0000

    Bash execute example - minor addition snapshot.

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,6 +8,18 @@ template '/var/www/html/index.html' do
 end
 
 
+# note: this works, but is not platform agnostic - this implies only 'linux' or machines that run with this setup.
+bash 'inline script' do
+    user "root"
+    code <<-EOH
+    mkdir -p /var/www/mysites/
+    chown -R apache /var/www/mysites/
+    EOH
+    not_if '[ -d /var/www/mysites/ ]'
+    action :run
+end
+
+
 service 'httpd' do
     action [ :enable, :start ]
 end
