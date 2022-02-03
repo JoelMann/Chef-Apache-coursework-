325702aab9215a5fbd55498a3bf58f8e733ea2e4
Joel Mann <jmann.creston@gmail.com>
2022-02-03 18:14:16 +0000

    Added using Directory, included examples for scripts

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,15 +8,41 @@ template '/var/www/html/index.html' do
 end
 
 
-# note: this works, but is not platform agnostic - this implies only 'linux' or machines that run with this setup.
-bash 'inline script' do
-    user "root"
-    code <<-EOH
-    mkdir -p /var/www/mysites/
-    chown -R apache /var/www/mysites/
-    EOH
-    not_if '[ -d /var/www/mysites/ ]'
-    action :run
+# note: this works, but is not platform agnostic - this implies only 'bash inclusive' or machines that run with this setup.
+# bash 'inline script' do
+#     user "root"
+#     code <<-EOH
+#     mkdir -p /var/www/mysites/
+#     chown -R apache /var/www/mysites/
+#     EOH
+#     not_if '[ -d /var/www/mysites/ ]'
+#     action :run
+# end
+
+# Execute will 'smart find' what the avail options are, will let chef figure out the way.
+
+# execute 'run a script' do
+#     user 'root'
+#     command <<-EOH
+#     mkdir -p /var/www/mysites/ /
+#     chown -R apache /var/www/mysites/
+#     EOH
+#     action :run
+# end
+
+# execute 'run script' do
+#     user 'root'
+#     command './myscript.sh'
+#     action :run
+# end
+
+#For our purposes, it's better to do this - this will test/fix/bring into management config.
+directory '/var/www/mysites' do
+    owner 'apache'
+    group 'apache'
+    recursive true
+    mode '0755'
+    action :create
 end
 
 
